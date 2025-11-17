77d86bdccc46f3ad55795e11a83f53acaeb78468
william <williamrutynamacedo@gmail.com>
2025-11-17 16:06:05 -0300

    Fix handler signature and make db init non-blocking

--- api/index.py
+++ api/index.py
@@ -1,4 +1,9 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app import app
 
-def handler(request):
-    return app(request.environ, request.start_response)
+def handler(request, response):
+    return app(request.environ, response.start_response)
